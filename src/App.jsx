f7e674a522fb365c3c433e7028c158a635c3edb9
Anjali <anjali000010@gmail.com>
2024-05-04 22:38:01 +0530

    added dropdown and filters

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,50 @@
 import Filter from "./components/Filter/Filter.jsx";
 import Badge from "./components/Badge/Badge.jsx";
-import './App.css'
+import "./App.css";
+import { useState, useEffect } from "react";
+import Card from "./components/Card/Card.jsx";
 
 function App() {
-  
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://api.weekday.technology/adhoc/getSampleJdJSON",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            limit: 10,
+            offset: 0,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+
+      const json = await response.json();
+      setData(json?.jdList);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
-    <Badge />
-    <Filter />
+      <Badge />
+      <Filter data={data} />
+      <Card data={data} />
     </div>
   );
 }
 
-export default App
+export default App;
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,79 +1,110 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import Dropdown from "../Dropdown/Dropdown";
 import "./Filter.css";
 
-const Filter = () => {
-  const filterData = [
-    {
-      name: "Number Of Employee",
-      options: [
-        "1-10",
-        "11-20",
-        "21-50",
-        "51-100",
-        "101-200",
-        "201-500",
-        "500+",
-      ],
-    },
-    {
-      name: "Experience",
-      options: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
-    },
-    { name: "Remote", options: ["Remote", "Hybrid", "In-office"] },
-    {
-      name: "Minimum Base Pay Salary",
-      options: ["0L", "10L", "20L", "30L", "40L", "50L", "60L", "70L"],
-    },
-  ];
-
-  /*{ name: 'Roles', options: [{category: 'ENGINEERING', options: ['Backend', 'Frontend', 'IOS', 'Flutter', 'React Native', 'Android', 'Tech Lead', 'Data Engineer', 'Data science', 'Computer Vision', 'Nlp', 'Deep-Learning']},
-      {category: 'DESIGN', options: ['Graphic Designer']},
-      {category: 'PRODUCT', options: ['Product Manager']},
-      {category: 'OPERATIONS', options: ['Operation Manager', 'Founder Office / Chief of Staff']},
-      {category: 'SALES', options: ['Development Representative', 'Account Executive', 'Account Manager']},
-      {category: 'MARKETING', options: ['Digital Marketing', 'Growth Hacker']},
-      {category: 'HR', opeions: ['Hr']},
-      {category: 'FINANCE', options: ['finance']}] } */
+const Filter = ({ data }) => {
 
   const [filters, setFilters] = useState({});
+  const [filteredData, setFilteredData] = useState([]);
+  const [experience, setExperience] = useState(null);
+   const keys = ["jobRole", "location", "minJdSalary"];
 
-  const handleFilterChange = (event, filterName) => {
+  useEffect(() => {
+    setFilteredData([...data]);
+  }, [data]);
+
+  const maxOfMinExp = () => {
+    let max = Number.MIN_VALUE;
+    for (const ele of data) {
+      if (ele?.minExp > max) {
+        max = ele?.minExp;
+      }
+    }
+    return max;
+  };
+
+  const fillArray = () => {
+    const maxVal = maxOfMinExp();
+    const filledArray = Array.from({ length: maxVal + 1 }, (_, index) => index);
+    return filledArray;
+};
+
+  const handleFilterChange = (key, value) => {
     setFilters((prevFilters) => ({
       ...prevFilters,
-      [filterName]: event.target.value,
+      [key]: value,
     }));
   };
 
+  useEffect(() => {
+    setFilteredData(
+      data?.filter((item) => {
+        const filterMatches = Object.keys(filters).every(
+          (key) => item[key] === filters[key]
+        );
+        const experienceMatches = !experience || item?.minExp === experience;
+
+        return filterMatches && experienceMatches;
+      })
+    );
+  }, [data, filters, experience]);
+
+  // const separatedValues = keys.map(key => {
+  //     return {
+  //         label: key.charAt(0).toUpperCase() + key.slice(1), // Capitalize the label
+  //         options: [...new Set(apiRes.map(obj => obj[key]))] // Use Set to remove duplicates
+  //     };
+  // });
   return (
-    <div className="filter-container">
-      {filterData.map((filter) => (
-        <div key={filter.name}>
-          <Dropdown
-            placeholder={filter.name}
-            value={filters[filter.name]}
-            options={filter.options.map((option) => ({
-              value: option,
-              label: option,
-            }))}
-            onChange={(e) => handleFilterChange(e, filter.name)}
-          />
-        </div>
-      ))}
+    <>
+      <div className="filter-container">
+        {keys.map((key) => (
+          <div key={key}>
+            <Dropdown
+              placeholder={key.charAt(0).toUpperCase() + key.slice(1)}
+              value={filters[key]}
+              options={[...new Set(data?.map((item) => item[key]))]?.map(
+                (value) => ({
+                  value: value,
+                  label: value,
+                })
+              )}
+              onChange={(e) => handleFilterChange(key, e.target.value)}
+            />
+          </div>
+        ))}
+        <Dropdown
+          placeholder={"Experience"}
+          value={experience}
+          options={fillArray()}
+          onChange={setExperience}
+          isExperience
+        />
+        <ul>
+          {filteredData?.map((item) => (
+            <li key={item.jdUid}>
+              <p>Company Name: {item.companyName}</p>
+              <p>Job Role: {item.jobRole}</p>
+              <p>Location: {item.location}</p>
+              <p>Minimum Experience: {item?.minExp ? item?.minExp : 0}</p>
+              <p>
+                Minimum Salary:{" "}
+                {item?.minJdSalary ? item?.minJdSalary + "Lac" : "NA"}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
       <input
         className="searchbar"
         type="text"
         placeholder="Search Company Name"
       ></input>
-    </div>
+    </>
   );
 };
 
 export default Filter;
 
-/*  {option.category ? <div> 
-                <span>{option.category}</span>
-                {suboptions.map((suboption) => (
-                  <div key={option.value}>{option.label}</div>
-                ))} */
+
