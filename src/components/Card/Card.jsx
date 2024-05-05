1b75d7d4c437039bc3c55141eae6a13e2598828a
Anjali <anjali000010@gmail.com>
2024-05-05 10:33:52 +0530

    added cards and fetching data

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,1 +1,43 @@
+import React, { useState, useEffect } from "react";
+import "./Card.css";
 
+const Card = ({ data }) => {
+  const [cards, setCards] = useState([{}]);
+
+  console.log(data);
+
+  useEffect(() => {
+    setCards([...data]);
+  }, [data]);
+
+  return (
+    <div className="cardMain-container">
+      <div>
+        {cards.map((card) => (
+          <div className="Card-container">
+            <div className="subClass">
+              <img className="logo" src={card.logoUrl} alt="logo" />
+              <span>
+                <p>{card.companyName}</p>
+                <p>{card.jobRole}</p>
+                <p>{card.location}</p>
+              </span>
+            </div>
+
+            <p>
+              Estimated Salary:{card.salaryCurrencyCode}
+              {card.minJdSalary}-{card.maxJdSalary}
+            </p>
+            <p>About Company:</p>
+            <p>About us</p>
+            <p>{card.jobDetailsFromCompany}</p>
+            <p>Minimum Experience</p>
+            <p>{card.minExp} Years</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
